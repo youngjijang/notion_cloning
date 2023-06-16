85cecd04675663f0abd10ebed97864b8037faf79
youngji <youngji.jang804@gmail.com>
2023-06-16 23:17:36 +0900

    init

---
+++ src/main/java/com/example/notion_cloning/NotionCloningApplication.java
@@ -0,0 +1,13 @@
+package com.example.notion_cloning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotionCloningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotionCloningApplication.class, args);
+	}
+
+}
